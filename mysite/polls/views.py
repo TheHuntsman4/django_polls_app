83d63f3e6ed2de4585824f69cd8360a6305453be
TheHuntsman4 <anikethvij464@gmail.com>
2023-01-21 17:03:05 +0530

    project till part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from .models import Questions
 
 
 def index(request):
-    return HttpResponse("test")
+    latest_questions=Questions.objects.order_by('pub_date')[:5]
+    output=','.join([q.question_text for q in latest_questions])
+    return HttpResponse(output)
+
+def detail(request,question_id):
+    return HttpResponse("You're looking at question %s." %question_id)
+
+def results(request,question_id):
+    return HttpResponse("Your're looking at the results for the questin %s." %question_id)
+
+def votes(request,question_id):
+    return HttpResponse("You are voting on the question %s." %question_id)
